7b622df0384f6a8b506052620c6d05c59cf3d0d4
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-03-17 23:14:03 +0700

    first commit, custom routing, create controller, create model, validation & sanitize, flash message, update detele, query builder

--- src/core/Database.php
+++ src/core/Database.php
@@ -3,10 +3,23 @@
 class Database
 {
     private $conn;
+    private $tableName; //for query builder
+    private $column = [];
+
 
     public function __construct()
     {
         $this->conn = $this->setConnection();
+    }
+
+    public function setTableName($tableName)
+    {
+        $this->tableName = $tableName;
+    }
+
+    public function setColumn($column)
+    {
+        $this->column = $column;
     }
 
     protected function setConnection()
@@ -31,5 +44,90 @@ class Database
         $stmt = $this->conn->prepare($query);
         $stmt->execute($params);
         return $stmt;
+    }
+
+    public function get($params = [])
+    {
+        $column = implode(',', $this->column);
+        $query = "select $column from {$this->tableName}";
+        $paramValue = [];
+        if (!empty($params)) {
+            $query .= " where 1=1 ";
+            foreach ($params as $key => $value) {
+                $query .= " and {$key} = ? ";
+                array_push($paramValue, $value);
+            }
+        }
+
+        return $this->query($query, $paramValue);
+    }
+
+    public function insertData($data = [])
+    {
+        if (empty($data)) {
+            return false;
+        }
+
+        $columnValue = [];
+        $kolom = [];
+        $param = [];
+
+        foreach ($data as $key => $value) {
+            array_push($kolom, $key);
+            array_push($columnValue, $value);
+            array_push($param, "?");
+        }
+
+        $kolom = implode(', ', $kolom);
+        $param = implode(', ', $param);
+
+        $query = "insert into {$this->tableName} ($kolom) values ($param)";
+        return $this->query($query, $columnValue);
+    }
+
+    public function updateData($data = [], $param = [])
+    {
+        if (empty($data)) {
+            return false;
+        }
+
+        $columnValue = [];
+        $kolom = [];
+        $query = "update {$this->tableName}";
+
+        foreach ($data as $key => $value) {
+            array_push($kolom, $key . "= ?");
+            array_push($columnValue, $value);
+        }
+
+        $kolom = implode(", ", $kolom);
+        $query = $query . " set $kolom where 1=1 ";
+        $whereColumn = [];
+        foreach ($param as $key => $value) {
+            array_push($whereColumn, "and {$key} = ?");
+            array_push($columnValue, $value);
+        }
+
+        $whereColumn = implode(", ", $whereColumn);
+        $query = $query . $whereColumn;
+        return $this->query($query, $columnValue);
+    }
+
+    public function deleteData($param = [])
+    {
+        if (empty($param)) {
+            return false;
+        }
+
+        $query = "delete from {$this->tableName}";
+        $paramValue = [];
+
+        $query = $query . " where 1=1 ";
+        foreach ($param as $key => $value) {
+            $query .= " and {$key} = ? ";
+            array_push($paramValue, $value);
+        }
+
+        return $this->query($query, $paramValue);
     }
 }
--- src/models/Barang_model.php
+++ src/models/Barang_model.php
@@ -5,46 +5,81 @@ class Barang_model extends Database
     public function __construct()
     {
         parent::__construct();
+        $this->setTableName('barang');
+        $this->setColumn([
+            'barang_id',
+            'nama_barang',
+            'jumlah',
+            'harga_satuan',
+            'expire_date'
+        ]);
     }
 
     public function getAll()
     {
-        $query = "select * from barang";
-        return $this->query($query)->fetchAll();
+        // $query = "select * from barang";
+        // return $this->query($query)->fetchAll();
+        //with query builder
+        return $this->get()->fetchAll();
     }
 
     public function insert($data)
     {
-        $query = "insert into barang (nama_barang, jumlah, harga_satuan, expire_date) values (?,?,?,?)";
-        return $this->query($query, [
-            $data['nama_barang'],
-            $data['jumlah'],
-            $data['harga_satuan'],
-            $data['kadaluarsa']
-        ]);
+        // $query = "insert into barang (nama_barang, jumlah, harga_satuan, expire_date) values (?,?,?,?)";
+        // return $this->query($query, [
+        //     $data['nama_barang'],
+        //     $data['jumlah'],
+        //     $data['harga_satuan'],
+        //     $data['kadaluarsa']
+        // ]);
+
+        $table = [
+            'nama_barang' => $data['nama_barang'],
+            'jumlah' => $data['jumlah'],
+            'harga_satuan' => $data['harga_satuan'],
+            'expire_date' => $data['kadaluarsa']
+        ];
+
+        return $this->insertData($table);
     }
 
     public function getById($id)
     {
-        $query = "select * from barang where barang_id = ?";
-        return $this->query($query, [$id])->fetch();
+        // $query = "select * from barang where barang_id = ?";
+        // return $this->query($query, [$id])->fetch();
+        return $this->get(['barang_id' => $id])->fetch();
     }
 
     public function update($data)
     {
-        $query = "update barang set nama_barang = ?, jumlah = ?, harga_satuan = ?, expire_date = ? where barang_id = ?";
-        return $this->query($query, [
-            $data['nama_barang'],
-            $data['jumlah'],
-            $data['harga_satuan'],
-            $data['kadaluarsa'],
-            $data['id']
-        ]);
+        // $query = "update barang set nama_barang = ?, jumlah = ?, harga_satuan = ?, expire_date = ? where barang_id = ?";
+        // return $this->query($query, [
+        //     $data['nama_barang'],
+        //     $data['jumlah'],
+        //     $data['harga_satuan'],
+        //     $data['kadaluarsa'],
+        //     $data['id']
+        // ]);
+
+        $table = [
+            'nama_barang' => $data['nama_barang'],
+            'jumlah' => $data['jumlah'],
+            'harga_satuan' => $data['harga_satuan'],
+            'expire_date' => $data['kadaluarsa']
+        ];
+
+        $key = [
+            'barang_id' => $data['id']
+        ];
+
+        return $this->updateData($table, $key);
     }
 
     public function delete($id)
     {
-        $query = 'delete from barang where barang_id = ?';
-        return $this->query($query, [$id]);
+        // $query = 'delete from barang where barang_id = ?';
+        // return $this->query($query, [$id]);
+
+        return $this->deleteData(['barang_id' => $id]);
     }
 }
